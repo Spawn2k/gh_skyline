60003fff2997b83ad023d8b81274db35ea15f1f8
Duy Tuan <duytuanvn1986@gmail.com>
2023-09-19 11:06:55 +0200

    update header pic mousemove

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,60 +5,72 @@
   const DOM = {};
   DOM.imgEls = document.querySelectorAll('.header-pic img');
   DOM.headerEl = document.querySelector('header');
+  DOM.headerPicEl = document.querySelectorAll('.header-pic')
   // console.log(DOM);
   let xValue = 0;
   let yValue = 0;
-
+ console.log()
   // === INIT =============
   const init = () => {
     DOM.headerEl.addEventListener('mousemove', onMouseMove);
+    DOM.headerEl.addEventListener('mouseenter', onMouseEnter)
+    DOM.headerEl.addEventListener('mouseleave', onMouseLeave)
   };
 
   // === EVENTHANDLER =====
 
   const onMouseMove = (e) => {
-    const { clientX } = e;
-    const { clientY } = e;
-    const percent = calcPercent(clientX);
-    let max = percent;
-    max = Math.max(percent, 40);
-    if (percent > 40) {
-      max = Math.min(percent, 80);
-    }
-    xValue = clientX - window.innerWidth / 2;
-    // yValue = clientY - window.innerHeight / 2;
+    const mouseDelter = parseFloat(DOM.headerPicEl[0].dataset.mouseEnter) - e.clientX
 
-    DOM.imgEls.forEach((img, idx) => {
-      moveImageBackground(img, max, -35);
-      if (idx === 1) {
-        moveImageBackground(img, max, 20);
-      }
-    });
-  };
+    const maxDelta = window.innerWidth / 2;
+    const percent = (mouseDelter / maxDelta) * -100
+    
+    let nextPercentage = parseFloat(DOM.headerPicEl[0].dataset.prevPercent) + percent
+    
+    console.log(nextPercentage)
+    nextPercentage = Math.min(nextPercentage, 17);
+    nextPercentage = Math.max(nextPercentage, -25);
+
+    DOM.headerPicEl.forEach((img) => {
+      moveImage(img, nextPercentage)
+    })
+    // console.log(nextPercentage)
+
+
+    // DOM.headerPicEl[0].dataset.percent = nextPercentage;
+
+    DOM.headerPicEl.forEach((img) => img.dataset.percent = nextPercentage);
+
+  }
+
+  const onMouseEnter = (e) => {
+    DOM.headerPicEl.forEach((el) => el.dataset.mouseEnter = e.clientX)
+    
+  }
+
+  const onMouseLeave = (e) => {
+    DOM.headerPicEl.forEach((el) => {
+      el.dataset.prevPercent = el.dataset.percent;
+    })
+  }
 
   // === XHR/FETCH ========
 
   // === FUNCTIONS ========
 
-  const calcPercent = (coordX) => {
-    const percent = (coordX / window.innerWidth) * 100;
+  
 
-    if (percent > 70 || percent < 20) return percent;
-    // console.log(percent);
-
-    return percent;
-  };
-
-  const moveImageBackground = (img, percent, offset = 0) => {
-    // console.log(percent);
-    img.style.setProperty('--cordX', `${percent + offset}%`);
-    // img.animate(
-    //   {
-    //     objectPosition: `${percent}% 0%`,
-    //   },
-    //   { fill: 'forwards', duration: 250, easing: 'ease-in' }
-    // );
-  };
+  const moveImage = (el, percent) => {
+    const image = el.querySelector('img');
+    const startPos = Number(el.dataset.startPos);
+    image.animate({
+      objectPosition: `${ startPos + (percent)}% 50%`,
+    },
+    {
+      duration: 300, fill: 'forwards'
+    })
+  }
+ 
 
   init();
 })();
